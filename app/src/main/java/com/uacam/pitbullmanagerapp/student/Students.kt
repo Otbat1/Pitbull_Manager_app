0e711644fc54c5a63922fd9201d53669b774770a
otbat <al061730@uacam.mx>
2022-05-23 13:04:16 -0500

    Creacion del metodo Students.kt

---
+++ app/src/main/java/com/uacam/pitbullmanagerapp/student/Students.kt
@@ -0,0 +1,11 @@
+package com.uacam.pitbullmanagerapp.student
+
+import java.io.Serializable
+
+data class Students (
+
+    var email: String? = null,
+    var first_name: String? = null,
+    var imagen: String? = null,
+    var last_name: String? = null,
+):Serializable
